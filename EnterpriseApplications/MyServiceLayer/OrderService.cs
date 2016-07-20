3c75a53b6d85ae5f66ef5b8ad9e0e018589c2e45
pdo9141 <pdo9141@gmail.com>
2016-07-19 22:33:04 -0700

    do it

---
+++ EnterpriseApplications/MyServiceLayer/OrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedContracts;
+
+namespace MyServiceLayer
+{
+    public class OrderService
+    {
+        public OrderServiceFindByIDResponse FindByID(OrderServiceFindByIDRequest request)
+        {
+            return null;
+        }
+    }
+}
---
+++ EnterpriseApplications/MyUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyServiceLayer;
+using SharedContracts;
+
+namespace MyUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OrderService_FindByID_Test()
+        {
+            OrderService service = new OrderService();
+            OrderServiceFindByIDRequest request = new OrderServiceFindByIDRequest();
+            OrderServiceFindByIDResponse response = service.FindByID(request);
+        }
+    }
+}
